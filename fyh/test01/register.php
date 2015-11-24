1f936551c4b25a310dc4dee55a3a7794481da3dd
fyhqqfyh <fyhqqfyh@outlook.com>
2015-11-24 08:23:44 +0800

    上传所有文件

---
+++ fyh/test01/register.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html> 
+<body>
+<form action="index.php" method="post">
+<h1>注册<h1>
+<h3>用户名：<input type="text" name="name"><br><br><h3>
+<h3>请设置密码 ：<input type="text" name="password"><br><br><h3>
+<h3>请确认密码 ：<input type="text" name="password"><br><br><h3>
+<input type="submit" name="submit" value="注册">
+<input type="submit" name="submit" value="取消"><br><br>
+</form>
+</body>
+</html>
