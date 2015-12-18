eb6e91a270a090ea257448960b021adc80cd74f9
fyhqqfyh <fyhqqfyh@outlook.com>
2015-12-18 20:17:33 +0800

    1

--- xiaobai/login/src/login.php
+++ xiaobai/login/src/login.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>注册</title>
+	<title>Login</title>
 	<style type="text/css">
 		div{
 			width:350px;
@@ -16,58 +16,56 @@
 	</style>
 </head>
 <body>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 	<div>
-		<h1>注册</h1></br></br>
-		&nbsp&nbsp&nbsp&nbsp用户名:<input type="text" name="username"></br>
-		请设置密码:<input type="password" name="password"></br>
-		请确认密码:<input type="password" name="password2"></br></br>
-		<input type="submit" value="注册">&nbsp&nbsp
+		<h1>Login</h1></br></br>
+		用户名:<input type="text" name="username"></br>
+		密码:&nbsp&nbsp<input type="password" name="password"></br></br>
+		<input type="submit" value="登录">&nbsp&nbsp
 		<input type="button" value="取消"></br></br>
-		<a href="login.php">注册成功返回登陆界面</a>
+		<a href="register.php">没有账户点击注册</a>
 	</div>
-	<?php
-	if(mysql_connect("localhost","root","root"))
+</form>
+</body>
+<?php
+if(mysql_connect("localhost","root","root"))
+{
+	//echo "连接成功";
+}else
+{
+	echo "连接失败";
+}
+if(mysql_select_db("test"))
+{
+	//echo "选择数据库成功";
+}else
+{
+	echo "选择数据库失败";
+}
+if(!empty($_POST))
+{
+	$username=$_POST["username"];
+	$password=$_POST["password"];
+	if($username=="" or $password=="" )
 	{
-		//echo "连接成功";
+		echo '<script>alert ("账号或密码不能为空!");</script>';
 	}else
 	{
-		echo "连接失败";
-	}
-	if(mysql_select_db("test"))
-	{
-		//echo "选择数据库成功";
-	}else
-	{
-		echo "选择数据库失败";
-	}
-
-	if(!empty($_POST))
-	{
-		$username=$_POST['username'];
-		$password=$_POST['password'];
-		$password2=$_POST['password2'];
-		if($username=="" or $password=="" or $password2=="")
+		$sql="select password from id where username='$username'";
+		$query=mysql_query($sql);
+		$arr=mysql_fetch_array($query);
+		if($arr=="")
 		{
-			echo "<script>alert('账号或密码不能为空！');</script>";
-		}elseif ($password!=$password2)
+			echo '<script>alert ("用户名不存在!");</script>';
+		}elseif($arr['password']==$password)
 		{
-			echo "<script>alert('两次输入密码不相同！');</script>";
+			echo "<script>alert ('登录成功!');</script>";
+			echo "<script>window.location.href='welcome.html'</script>";
 		}else
 		{
-			$sql="insert into id (username,password)
-		values('$_POST[username]','$_POST[password]')";
-			if (mysql_query($sql))
-			{
-				echo "<script>alert('注册成功！');</script>";
-				echo "<script>window.location.href='login.php'</script>";
-
-			}else
-			{
-				echo "<script>alert('用户名已占用！');</script>";
-			}
+			echo '<script>alert ("账号或密码错误!");</script>';
 		}
 	}
-	?>
-</body>
+}
+?>
 </html>
