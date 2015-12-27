bbb8e0f6a3de7db316b9a17c5358b5631606e14a
fyhqqfyh <fyhqqfyh@outlook.com>
2015-12-27 13:28:54 +0800

    2

---
+++ xiaobai/MVC/MVC/test.php
@@ -0,0 +1,7 @@
+<?php
+require_once ('testController.class.php');
+require_once ('testModel.class.php');
+require_once ('testView.class.php');
+$testController = new testController();
+$testController -> show();
+ 
