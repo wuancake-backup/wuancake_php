a74dcf0ad2457644a2d56e903ec1a713ae050bec
xiaoxinTOM <xiaoxin1995ok@gmail.com>
2016-01-06 19:33:42 +0800

    做作业4

--- xiaoxin/logup.php
+++ xiaoxin/logup.php
@@ -7,7 +7,7 @@ $db=mysql_connect("localhost","root","");
 mysql_select_db("log",$db) or die ("数据库选择失败");
 mysql_query("SET NAMES UTF8");
 if ($passone==$passtwo){
-$a=mysql_query("insert into logup values ($name,$passone,$passone);");
+$a=mysql_query("insert into logup values ('$name',$passone,$passone);");
 if ($a) {
 	echo "欢迎来到绅士的世界";
 }else{
