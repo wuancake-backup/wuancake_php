512b76e0a143cba26acab13dbc74a5e45772434b
aunhappy <1195417752@qq.com>
2016-01-26 09:41:04 +0800

    修改list.php

--- csc/message/list.php
+++ csc/message/list.php
@@ -12,13 +12,20 @@ while ($row=mysql_fetch_array($query)){ error_reporting(0);
 ?> 
 
 <tr bgcolor="#eff3ff"> 
-<td>标题：<font color="red"><?=$row[title]?></font> 用户：<font color="red"><?=$row[user] ?></font><div align="right"><a href="preEdit.php?id=<?=$row[id]?>">编辑</a>  |  <a href="delete.php?id=<?=$row[id]?>">删除</a></div></td> 
+       <td>标题：<font color="red"><?=$row[title]?></font> 
+       用户：<font color="red"><?=$row[user] ?></font>
+       <div align="right">
+               <a href="preEdit.php?id=<?=$row[id]?>">编辑</a>
+             |
+			   <a href="delete.php?id=<?=$row[id]?>">删除</a>
+       </div>
+       </td> 
 </tr> 
 <tr bgColor="#ffffff"> 
 <td>内容：<?=$row[content]?></td> 
 </tr> 
 <tr bgColor="#ffffff"> 
-<td><div align="right">发表日期：<?=$row[lastdate]?></div></td> 
+<td><div align="right">发表日期：<?php echo substr($row[lastdate],0,16)?></div></td> 
 </tr> 
 <?php }?> 
 </table>
