ed5fd3a3b40b6ed20a078f43a2c696b067a02141
aunhappy <1195417752@qq.com>
2016-02-03 21:41:49 +0800

    messageupdate

--- csc/message v2/list.php
+++ csc/message v2/list.php
@@ -1,17 +1,5 @@
 <META http-equiv="content-type" content="text/html; charset=utf-8">
-<script language="javascript">
-    function submit1()
-    {
-    document.getElementById("submitform").action="preEdit.php";
-    document.getElementById("submitform").submit();
-    }
-    
-    function submit2()
-    {
-    document.getElementById("submitform").action="delete.php";
-    document.getElementById("submitform").submit();
-    }
-</script>
+
 <?php 
 include 'conn.php'; 
 ?> 
@@ -29,29 +17,56 @@ $sql="SELECT pb.title,pd.ID,pd.text,MAX(pd.createTime) AS createTime,ub.name
                     $result = mysql_query($sql);
                     while($row = mysql_fetch_array($result))
                     {
+                    	//var_dump($row);
                          error_reporting(0);
 ?> 
 
+<script language="javascript">
+    function submit<?php echo $row[ID]?>()
+    {
+        var preEditForm = "submitform"+<?php echo $row[ID]?>;
+        document.getElementById(preEditForm).action="preedit.php";
+        document.getElementById(preEditForm).submit();
+    }
+    
+    function submi<?php echo $row[ID]?>()
+    {    
+	     var deleteform = "submitform"+<?php echo $row[ID]?>;
+    document.getElementById(deleteform).action="delete.php";
+    document.getElementById(deleteform).submit();
+    }
+	function reply()
+	{
+	window.location.href='reply.php?id=<?php echo $row[ID]?>';
+	}
+</script>
+
 <tr bgcolor="#eff3ff"> 
-       <td>标题：<font color="red"><?=$row[title]?></font> 
-       用户：<font color="red"><?=$row[name] ?></font>
+       <td>标题：<font color="red"><?php echo $row[title]?></font> 
+       用户：<font color="red"><?php echo $row[name] ?></font>
        <div align="right">
                         
 
-			 <form name="submitform" id="submitform" method="post" action="">
+			 <form name="submitform<?php echo $row[ID]?>" id="submitform<?php echo $row[ID]?>" method="post" action="">
 			 
-			  <input type="hidden" name="id" id="ID" value="<?=$row[ID]?>">
-			  <INPUT TYPE="button" name="preEdit" id="preEdit" value="编辑" onclick="submit1()"/>
-			  |
-			  <INPUT TYPE="button" name="delete" id="delete" value="删除" onclick="submit2()"/>
-			   | <a href="reply.php?id=<?=$row[ID]?>">回复</a>
-			     </FORM>
+			  <input type="hidden" name="id" id="id" value="<?php echo $row[ID]?>"> 
+			  <?php session_start(); if($row[name]===$_SESSION['username'])
+			  {
+			  echo "<INPUT TYPE='button' name='preEdit' id='preEdit' value='编辑' onclick='submit<?php echo $row[ID]?>()'/>";
+			  echo " | "; 
+			  echo "<INPUT TYPE='button' name='delete' id='delete' value='删除' onclick='submi<?php echo $row[ID]?>()'/>";
+			  echo " | ";
+			  echo "<input type='button' name='reply' id='reply' value='回复' onclick='window.location.href=\"reply.php?id=$row[ID]\"'/> ";
+			  }else{
+			  echo "编辑 | 删除 | <a href='reply.php?id=$row[ID]'>回复</a>";}
+			  ?>
+			 </form>
 			   
        </div>
        </td> 
 </tr> 
 <tr bgColor="#ffffff"> 
-<td>内容：<?=$row[text]?></td> 
+<td>内容：<?php echo $row[text]?></td> 
 </tr> 
 <tr bgColor="#ffffff"> 
 <td><div align="right">发表日期：<?php echo substr($row[createTime],0,16)?></div></td> 
--- csc/message v2/list2.php
+++ csc/message v2/list2.php
@@ -1,10 +1,22 @@
-<META http-equiv="content-type" content="text/html; charset=utf-8"><title>留言首页</title>
-
+<META http-equiv="content-type" content="text/html; charset=utf-8">
+<script language="javascript">
+    function submit1()
+    {
+    document.getElementById("submitform").action="preEdit.php";
+    document.getElementById("submitform").submit();
+    }
+    
+    function submit2()
+    {
+    document.getElementById("submitform").action="delete.php";
+    document.getElementById("submitform").submit();
+    }
+</script>
 <?php 
-include 'conn.php'; error_reporting(0);
+include 'conn.php'; 
 ?> 
 <?php 
-echo "<div align='center'><a href='add.php'>继续添加</a>"; echo " <a href='login.php?action=logout'>登录/登出</a></div>";
+echo "<div align='center'><a href='add.php'>继续添加</a></div>"; 
 ?> 
 <table width=500 border="0" align="center" cellpadding="5" cellspacing="1" bgcolor="#add3ef"> 
 <?php 
@@ -16,7 +28,7 @@ $sql="SELECT pb.title,pd.ID,pd.text,MAX(pd.createTime) AS createTime,ub.name
                          // LIMIT $limit_st,$page_num";
                     $result = mysql_query($sql);
                     while($row = mysql_fetch_array($result))
-                    {
+                    {    
                          error_reporting(0);
 ?> 
 
@@ -26,13 +38,15 @@ $sql="SELECT pb.title,pd.ID,pd.text,MAX(pd.createTime) AS createTime,ub.name
        <div align="right">
                         
 
-			     
-			  <?php session_start(); if($row[name]===$_SESSION['username']){
-			  echo "<a href='preedit.php?id=$row[ID]'>编辑</a>"; 
-			  echo "|";echo "<a href='delete.php?id=$row[ID]'>删除</a>";}else{
-			  echo "编辑|删除";
-			  
-			  }?>|<a href="reply.php?id=<?=$row[ID]?>">回复</a>
+			 <form name="submitform" id="submitform" method="post" action="">
+			 
+			  <input type="hidden" name="id" id="ID" value="<?=$row[ID]?>">
+			  <INPUT TYPE="button" name="preEdit" id="preEdit" value="编辑" onclick="submit1()"/>
+			  |
+			  <INPUT TYPE="button" name="delete" id="delete" value="删除" onclick="submit2()"/>
+			   | <a href="reply.php?id=<?=$row[ID]?>">回复</a>
+			     </FORM>
+			   
        </div>
        </td> 
 </tr> 
