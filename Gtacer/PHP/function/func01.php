a339d71a7a817a2ce06ba4423683c025b1534dd8
Gtacer <459472218@qq.com>
2017-03-01 18:54:10 +0800

    函数

---
+++ Gtacer/PHP/function/func01.php
@@ -0,0 +1,23 @@
+<?php
+	function jisuan($num1,$num2,$oper){
+		$res=0;
+		switch($oper){
+			case "+":
+				$res=$num1+$num2;
+				break;
+			case "-":
+				$res=$num1-$num2;
+				break;
+			case "*":
+				$res=$num1*$num2;
+				break;
+			case "/":
+				$res=$num1/$num2;
+				break;
+			default:
+				echo 'ÄãµÄÊäÈëÓÐÎó';
+				break;
+		}
+	return $res;
+	}
+?>
---
+++ Gtacer/PHP/function/func02.php
@@ -0,0 +1,11 @@
+<html>
+<?php
+	require 'func01.php';//引入需要的php文件
+
+	$num1=34;
+	$num2=59;
+	$oper="/";
+	$res=jisuan($num1,$num2,$oper);
+	echo '计算结果是'.$res;
+?>
+</html>
