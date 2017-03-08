657e049f4ad0e0b55909079ae9959ff45eb2868a
Gtacer <459472218@qq.com>
2017-03-08 22:38:36 +0800

    对象的传递方式

--- Gtacer/PHP/类与对象/person.class.php
+++ Gtacer/PHP/类与对象/person.class.php
@@ -1,9 +1,9 @@
 <?php
-    class person{
+    class person{       //定义类
         public $name;
         public $age;
     }
-    $a=new person();
+    $a=new person();       //定义一个新的对象
     $a->name="小明";
     $a->age=20;
 
@@ -11,4 +11,10 @@
 
     echo $a->name."<br/>";
     echo $b->name."<br/>";
+
+    function test($p){      //对象按地址传递到函数中
+        $p->name="小红";
+    }
+    test($a);
+    echo $a->name;
 ?>
