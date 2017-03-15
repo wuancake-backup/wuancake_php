fcdfecc82b6bafb48b8f38c2eb2cfd1fab64df04
Gtacer <459472218@qq.com>
2017-03-15 09:48:29 +0800

    在网页下载指定的文件

---
+++ Gtacer/PHP/http协议/文件下载/file/downlist.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <title>下载</title>
+</head>
+<a href='down.php?filenname=1.jpg'>点击下载图片1</a><img src="./picture/1.jpg" width="80px"/><br/><br/><br/>
+<a href='down.php?filenname=2.jpg'>点击下载图片2</a><img src="./picture/2.jpg" width="80px"/><br/><br/><br/>
+<a href='down.php?filenname=3.jpg'>点击下载图片3</a><img src="./picture/3.jpg" width="80px"/><br/><br/><br/>
+<a href='down.php?filenname=4.jpg'>点击下载图片4</a><img src="./picture/4.jpg" width="80px"/><br/><br/><br/>
+<a href='down.php?filenname=5.jpg'>点击下载图片5</a><img src="./picture/5.jpg" width="80px"/><br/><br/><br/>
+<a href='down.php?filenname=6.jpg'>点击下载图片6</a><img src="./picture/6.jpg" width="80px"/><br/><br/><br/>
+</html>
