082f28896fbca9564799d14511836bd655d65418
Gtacer <459472218@qq.com>
2017-04-08 00:19:31 +0800

    close

--- Gtacer/PHP/mysqli/mysqliQuery.php
+++ Gtacer/PHP/mysqli/mysqliQuery.php
@@ -9,3 +9,4 @@
     $sqls.="INSERT INTO test(name,password) VALUE ('xiao333',md5(123456));" ;
     $res=$mysqli->multi_query($sqls) or die('添加失败'.$mysqli->error);
     echo '添加成功';
+    $mysqli->close();
