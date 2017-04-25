00cff329aa5f27e0e9b7bd3c7768b30f92fd97f5
Gtacer <459472218@qq.com>
2017-04-25 21:04:04 +0800

    修饰

--- Gtacer/PHP/魔术方法/clone.php
+++ Gtacer/PHP/魔术方法/clone.php
@@ -1,22 +1,22 @@
 <?php
     class MyClass
     {
-    public $name;
-    public $age;
+		public $name;
+		public $age;
 
-    public function hello(){
-    echo 'hello';
-    }
+		public function hello(){
+		echo 'hello';
+		}
 
-    //当复制完成时，
-    //如果定义了 __clone() 方法，则新创建的对象（复制生成的对象）中的 __clone() 方法会被调用，
-    //可用于修改属性的值（如果有必要的话）。
-    public function __clone(){
-    echo 'hello';
-    $this->name='小红';
-    $this->age=80;
-    }
-    }
+		//当复制完成时，
+		//如果定义了 __clone() 方法，则新创建的对象（复制生成的对象）中的 __clone() 方法会被调用，
+		//可用于修改属性的值（如果有必要的话）。
+		public function __clone(){
+		echo 'hello';
+		$this->name='小红';
+		$this->age=80;
+		}
+	}
 
     $a=new MyClass;
     $a->name='Jone';
