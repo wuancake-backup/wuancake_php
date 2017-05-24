797ee2cc20db5711f62925c1f2bf3b02e5789339
Gtacer <459472218@qq.com>
2017-05-25 00:15:43 +0800

    使用ajax实现在不刷新页面的情况下切换分页

---
+++ Gtacer/PHP/不刷新切换分页/page.php
@@ -0,0 +1,64 @@
+<?php
+    $connect = new mysqli('localhost','root','root','test');
+    if ($connect->connect_error)
+        die('连接失败'.$connect->connect_error);
+
+    $size_all = $connect->query('SELECT id FROM foo');
+    $size = ceil($size_all->num_rows/5);
+    
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    $offset = ($page - 1)*5;
+
+    $res = $connect->query("SELECT * FROM foo LIMIT $offset,5");
+    ?>
+        <html>
+        <body>
+        <table border="1">
+            <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>age</th>
+            </tr>
+            <?php
+            while ($result = $res->fetch_object()){
+                echo "<tr>";
+                foreach ($result as $value){
+                    echo "<td>";
+                    echo "$value";
+                    echo "</td>";
+                }
+                echo "</tr>";
+            }
+            ?>
+            <tr>
+                <td colspan='3'>
+                    <a href="javascript:showPage(1)">首页</a>
+                    <a href="javascript:showPage(<?php if ($page==1)echo 1;else echo $page-1?>)">上一页</a>
+                    <?php
+                    for ($i = $page <= 3 ? 1 : $page - 2;$i <= $size;$i++){
+                        if ($i == $page)
+                            echo "<a href='javascript:showPage($i)' style='color: red'>[$i]</a>&nbsp;";
+                        else
+                            echo "<a href='javascript:showPage($i)'>[$i]</a>&nbsp;";
+                        if ($page <= 3){
+                            if ($i == 5)
+                                break;
+                        }
+                        else
+                            if($i > $page + 1)
+                                break;
+                    }
+                    ?>
+                    <a href="javascript:showPage(<?php if ($page==$size)echo $size;else echo $page+1?>)">下一页</a>
+                    <a href="javascript:showPage(<?php echo $size?>)">最后一页</a>
+                </td>
+            </tr>
+        </table>
+        </body>
+        </html>
+    <?php
+    $size_all->free_result();
+    $res->free_result();
+    $connect->close();
+
+    ?>
